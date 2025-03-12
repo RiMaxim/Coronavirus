70317119d1a9d5f7778890a0c79c6c4fc2feacfb
RI MAXIM <maxim.ree@googlemail.com>
2025-03-12 19:34:02 +0200

    Add files via upload

--- separate_length.sh
+++ separate_length.sh
@@ -1,9 +1,9 @@
 cat $1 |awk -F'\t' '{if(length($1) > '7' && length($1) < '17') print $1}' >tmp1
-cat tmp1 |awk -F'\t' '{if(length($1) == '8') print $1}' >length_8
-cat tmp1 |awk -F'\t' '{if(length($1) == '9') print $1}' >length_9.1
-awk '{print substr($1,2); }' length_9.1 >length_9.2
+cat tmp1 |awk -F'\t' '{if(length($1) == '8') print $1}' >8.length
+cat tmp1 |awk -F'\t' '{if(length($1) == '9') print $1}' >9.length
+awk '{print substr($1,2); }' 9.length >8_9.length
 cat tmp1 |awk -F'\t' '{if(length($1) > '9') print $1}' >tmp2
-awk '{print substr($1,length($1)-7,8) }' tmp2 >length_10.3
-awk '{print substr($1,length($1)-8,9) }' tmp2 >length_10.2
-awk '{print substr($1,length($1)-9,10) }' tmp2 >length_10.1
+awk '{print substr($1,length($1)-7,8) }' tmp2 >8_10.length
+awk '{print substr($1,length($1)-8,9) }' tmp2 >9_10.length
+awk '{print substr($1,length($1)-9,10) }' tmp2 >10.length
 rm tmp*
