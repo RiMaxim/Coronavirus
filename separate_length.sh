9199b3214c74afa5c257b6763c8fa4b5dda0ef84
RI MAXIM <maxim.ree@googlemail.com>
2025-04-14 23:40:54 +0200

    Update separate_length.sh

--- separate_length.sh
+++ separate_length.sh
@@ -1,4 +1,10 @@
-cat $1 |awk -F'\t' '{if(length($1) > '7' && length($1) < '17') print $1}' >tmp1
+cat $1 |awk -F',' '{if($6 == "Wuhan-Hu-1 and Omicron B.1.1.529") print $1}' >>wt.list
+cat $1 |awk -F',' '{if($6 == "Wuhan-Hu-1") print $1}' >>wt.list
+
+cat $1 |awk -F',' '{if($6 == "Wuhan-Hu-1 and Omicron B.1.1.529") print $1}' >>omicron.list
+cat $1 |awk -F',' '{if($6 == "Omicron B.1.1.529") print $1}' >>omicron.list
+
+cat $2 |awk -F'\t' '{if(length($1) > '7' && length($1) < '17') print $1}' >tmp1
 cat tmp1 |awk -F'\t' '{if(length($1) == '8') print $1}' >8.length
 cat tmp1 |awk -F'\t' '{if(length($1) == '9') print $1}' >9.length
 awk '{print substr($1,2); }' 9.length >8_9.length
@@ -6,4 +12,4 @@ cat tmp1 |awk -F'\t' '{if(length($1) > '9') print $1}' >tmp2
 awk '{print substr($1,length($1)-7,8) }' tmp2 >8_10.length
 awk '{print substr($1,length($1)-8,9) }' tmp2 >9_10.length
 awk '{print substr($1,length($1)-9,10) }' tmp2 >10.length
-rm tmp*
+rm tmp* omicron.list wt.list
