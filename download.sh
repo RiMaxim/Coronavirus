6d6a7d1435055f7ffba8a6428ff6861cb9687575
RI MAXIM <maxim.ree@googlemail.com>
2025-04-14 15:50:53 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,4 @@
+cat table.txt |awk -F'\t' '{print $1}' >ids.txt
 while read -r id; do
     wget -q -O - "http://www.allelefrequencies.net/tools/getrawdata.asp?pop_id=${id}" >> data.csv
 done < ids.txt
