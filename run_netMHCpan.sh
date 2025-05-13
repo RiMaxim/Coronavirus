215160ec4c054661cbc29ae0f68339ab57d21ba6
RI MAXIM <maxim.ree@googlemail.com>
2025-05-13 21:46:11 +0200

    Update run_netMHCpan.sh

--- run_netMHCpan.sh
+++ run_netMHCpan.sh
@@ -4,9 +4,13 @@ while read -r id; do
     ./netMHCpan -p tmp1 -a ${id} >>tmp2
 done < data4.csv
 
-grep "PEPLIST" tmp2 |grep -v "Protein" |sed 's/ /|/g'|sed 's/|||||||||/\t/g'| sed 's/|||||||/\t/g'| sed 's/||||||/\t/g'| sed 's/|||||/\t/g'| sed 's/||||/\t/g'| sed 's/|||/\t/g'| sed 's/||/\t/g'| sed 's/|/\t/g' |awk -F'\t' '{print $4"\t"$3"["$14"]""\t"$3"\t"$14}' | \
-awk -F'\t' '{if($4 <= '$1') print $1"\t"$3}' | \
-awk -F'\tHLA-' '{print $2}' | sort |uniq >data5.csv
+grep "PEPLIST" tmp2 |grep -v "Protein" |sed 's/ /|/g'|sed 's/|||||||||/\t/g'| sed 's/|||||||/\t/g'| sed 's/||||||/\t/g'| sed 's/|||||/\t/g'| sed 's/||||/\t/g'| sed 's/|||/\t/g'| sed 's/||/\t/g'| sed 's/|/\t/g' |awk -F'\t' '{print $4"\t"$3"\t"$14}' | \
+awk -F'\t' '{if($3 <= '$1') print $1"\t"$2}' >tmp3
+
+cut -f 1 tmp3 >tmp4
+cut -f 2 tmp3 |awk -F'\tHLA-' '{print $2}' | sort |uniq >tmp5
+
+paste tmp4 tmp5 >data5.csv
 
 rm tmp*
 
