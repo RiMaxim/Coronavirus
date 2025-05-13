9cc2484fb2ea58d6803972d599ff5890f2f74427
RI MAXIM <maxim.ree@googlemail.com>
2025-05-13 22:07:38 +0200

    Update run_netMHCpan.sh

--- run_netMHCpan.sh
+++ run_netMHCpan.sh
@@ -10,7 +10,7 @@ awk -F'\t' '{if($3 <= '$1') print $1"\t"$2}' >tmp3
 cut -f 1 tmp3 >tmp4
 cut -f 2 tmp3 |awk -F'HLA-' '{print $2}' | sort |uniq >tmp5
 
-paste tmp4 tmp5 >data5.csv
+paste tmp5 tmp4 |awk -F'\t' '{print $1","$2}' >data5.csv
 
 rm tmp*
 
