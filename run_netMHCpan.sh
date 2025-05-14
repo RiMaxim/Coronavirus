8c128de406f4d50d43155c9a358d1d0620b44063
RI MAXIM <maxim.ree@googlemail.com>
2025-05-14 10:14:33 +0200

    Add files via upload

--- run_netMHCpan.sh
+++ run_netMHCpan.sh
@@ -1,18 +1,56 @@
-cat 10.length 8.length 8_10.length 8_9.length 9.length 9_10.length |sort|uniq >tmp1
+cat 10.length 8.length 8_10.length 8_9.length 9.length 9_10.length |awk -F'\t' '{print $1","$2}' >tmp1
 
-while read -r id; do
-    ./netMHCpan -p tmp1 -a ${id} >>tmp2
-done < data4.csv
+awk -F',' 'NR > 1 {
+    peptide_full = $2;
+    peptide = $1;
+    peptide_list[peptide_full] = (peptide_list[peptide_full] == "" ? peptide : peptide_list[peptide_full] "\n" peptide);
+}
+END {
+    for (pep in peptide_list) {
+        out_file = "list/" pep;
+        gsub(/[^A-Za-z0-9_]/, "_", out_file);
+        print peptide_list[pep] > out_file;
+    }
+}
+' tmp1
 
-grep "PEPLIST" tmp2 |grep -v "Protein" |sed 's/ /|/g'|sed 's/|||||||||/\t/g'| sed 's/|||||||/\t/g'| sed 's/||||||/\t/g'| sed 's/|||||/\t/g'| sed 's/||||/\t/g'| sed 's/|||/\t/g'| sed 's/||/\t/g'| sed 's/|/\t/g' |awk -F'\t' '{print $4"\t"$3"\t"$14}' | \
-awk -F'\t' '{if($3 <= '$1') print $1"\t"$2}' >tmp3
+max_jobs=20
+current_jobs=0
 
-cut -f 1 tmp3 >tmp4
-cut -f 2 tmp3 |awk -F'HLA-' '{print $2}' >tmp5
+for pepfile in list_*; do
+    (
+        outfile="$(basename "${pepfile%.*}").out"
+        > "${outfile}"
+        while IFS= read -r hla; do
+            hla=$(echo "$hla" | xargs)
+            [[ -z "$hla" ]] && continue
+            echo ">>> HLA: ${hla}" >> "${outfile}"
+            ./netMHCpan -p "${pepfile}" -a "${hla}" >> "${outfile}"
+            echo "" >> "${outfile}"
+        done < data4.csv
+    ) &
 
-paste tmp5 tmp4 |awk -F'\t' '{print $1","$2}' >data5.csv
+    ((current_jobs++))
+    if (( current_jobs >= max_jobs )); then
+        wait -n
+        ((current_jobs--))
+    fi
+done
 
-rm tmp*
+wait
+
+for pepfile in list_*.out; do
+    tag=$(basename "$pepfile" .out)
+    tag=${tag#list_}
+    outfile="$(basename "${pepfile}").HLA"
+    grep "PEPLIST" ${pepfile} |grep -v "Protein" |sed 's/ /|/g'|sed 's/|||||||||/\t/g'| sed 's/|||||||/\t/g'| sed 's/||||||/\t/g'| sed 's/|||||/\t/g'| sed 's/||||/\t/g'| sed 's/|||/\t/g'| sed 's/||/\t/g'| sed 's/|/\t/g' |awk -F'\t' '{print $3"\t"$14}' | \
+    awk -F'\t' '{if($2 <= '$1') print $1}' | \
+    awk -F'HLA-' '{print $2}' |sort|uniq |awk -F'\t' -v tag="$tag" '{print $1 "\t" tag}'  >${outfile}
+    cat ${outfile} >>tmp2
+done
+
+cat tmp2 |awk -F'\t' '{print $1","$2}' >data5.csv
+rm tmp* list*
 
 
 
